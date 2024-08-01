520c095ffa4e97c64662eecc9f74af523f6ee275
Турсунов Баходур <tursunovb18@gmail.com>
2024-08-01 09:17:01 +0500

    added project with relationships and added migration to db

---
+++ BaseLibrary/DTOs/AccountBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaseLibrary.DTOs
+{
+    public class AccountBase
+    {
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        [Required]
+        public string? Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [EmailAddress]
+        [Required]
+        public string? Password { get; set; }
+    }
+}
---
+++ ServerLibrary/Repositories/Implementation/UserAccountRepository.cs
@@ -0,0 +1,31 @@
+﻿using BaseLibrary.DTOs;
+using BaseLibrary.Entities;
+using BaseLibrary.Responses;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ServerLibrary.Data;
+using ServerLibrary.Helpers;
+using ServerLibrary.Repositories.Contracts;
+
+namespace ServerLibrary.Repositories.Implementation
+{
+    internal class UserAccountRepository(IOptions<JwtSection> config, AppDbContext appDbContext) : IUserAccount
+    {
+
+        public async Task<GeneralResponse> CreateAsync(Register user)
+        {
+            if (user is null) return new GeneralResponse(false, "Model is empty");
+
+            var checkUser = await FindUserByEmail(user.Email!);
+            if (checkUser != null) return new GeneralResponse(false, "User registered already");
+
+        }
+
+        public async Task<LoginResponse> CreateAsync(Login user)
+        {
+
+        }
+
+        private async Task<ApplicationUser> FindUserByEmail(string email) => await appDbContext.ApplicationUsers.FirstOrDefaultAsync(_ => _.Email!.ToLower()!.Equals(email!.ToLower()));
+
+    }
